5e16b06697ce6fdb58cdd7376c69f9c75cf47239
programmeracid <142600387+programmeracid@users.noreply.github.com>
2025-09-19 22:39:47 +0530

    Firebase init modified

--- app/firebase_init.py
+++ app/firebase_init.py
@@ -2,21 +2,29 @@ import os
 from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os, dotenv
+import json
 
 # Load environment variables
 load_dotenv()
 
 def firestore_init():
 
+
+
     # --- Firestore setup (requires service account) ---
     # In backend, you need a service account to actually connect.
     # Download serviceAccountKey.json from Firebase Console > Settings > Service accounts
     # Then load it securely (better: use env vars instead of raw file)
 
-    service_account_path = "placement-tracker-c7d72-ae8bf36ecac6.json"  # keep this in .gitignore
+    firebase_json = os.environ.get("FIREBASE_CREDENTIALS")
 
+    if not firebase_json:
+        firebase_cred = "placement-tracker-c7d72-ae8bf36ecac6.json"  # keep this in .gitignore
+    else:
+        firebase_cred = json.loads(firebase_json)
     if not firebase_admin._apps:
-        cred = credentials.Certificate(service_account_path)
+        cred = credentials.Certificate(firebase_cred)
         firebase_admin.initialize_app(cred)
 
     db = firestore.client()
@@ -27,4 +35,4 @@ def firestore_init():
 
 
 if __name__ == "__main__":
-    print('nob')
+    firestore_init()
